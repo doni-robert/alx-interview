8edd58d8cfee8819394d5b132a5904f9bca66a6f
doni-robert <irobertndungu@gmail.com>
2023-12-07 15:38:57 +0300

    task done

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Prime Game
+"""
+
+
+def isWinner(x, nums):
+    """
+    Determines the winner in the prime game
+    """
+    Ben = 0
+    Maria = 0
+
+    for round in range(x):
+        playing_numbers = [num for num in range(2, nums[round] + 1)]
+        index = 0
+        while (index < len(playing_numbers)):
+            current_prime = playing_numbers[index]
+            sieve_index = index + current_prime
+            while(sieve_index < len(playing_numbers)):
+                playing_numbers.pop(sieve_index)
+                sieve_index += current_prime - 1
+            index += 1
+
+        prime_count = (len(playing_numbers))
+        if prime_count and prime_count % 2:
+            Maria += 1
+        else:
+            Ben += 1
+
+    if Ben == Maria:
+        return None
+    return 'Ben' if Ben > Maria else 'Maria'
